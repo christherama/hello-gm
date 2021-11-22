711c45d165688d6ff14e89811d0a2a5cc7d9be0c
christherama <chris@rama.io>
2021-11-21 23:31:50 -0600

    Updates to patch decorator

--- api/tests/test_api.py
+++ api/tests/test_api.py
@@ -1,30 +1,56 @@
 import pytest
 from ariadne import make_executable_schema
 from ariadne import graphql_sync
+from patient import errors
 from unittest.mock import patch, Mock
 
 from hypothesis import given
 from hypothesis.strategies import integers
 
 
-@given(integers(min_value=1, max_value=10000))
-def test__get_patient__responds_with_patient_if_exists(schema, patient_id: int):
-    with patch("api.controller.patient_service") as mock_patient_service:
-        mock_patient = Mock(first_name="Chris", last_name="Ramacciotti")
-        mock_patient_service.get_patient_by_id.return_value = mock_patient
-        query = f"""
-            {{
-                patient(id: {patient_id}) {{
-                    first_name
-                    last_name
-               }}
-            }}
-        """
-        data = {
-            "query": query
-        }
-        success, response = graphql_sync(schema, data)
+@patch("api.controller.patient_service")
+def test__get_patient__responds_with_patient_if_exists(mock_patient_service, schema):
+    mock_patient = Mock(first_name="Chris", last_name="Ramacciotti")
+    mock_patient_service.get_patient_by_id.return_value = mock_patient
+    query = f"""
+        {{
+            patient(id: 1) {{
+                first_name
+                last_name
+           }}
+        }}
+    """
+    data = {
+        "query": query
+    }
+    success, response = graphql_sync(schema, data)
+    
     assert success
+
+    patient = response["data"]["patient"]
+    assert patient["first_name"] == mock_patient.first_name
+    assert patient["last_name"] == mock_patient.last_name
+
+
+@patch("api.controller.patient_service")
+def test__get_patient__responds_with_404_if_not_exists(mock_patient_service, schema):
+    mock_patient_service.get_patient_by_id.side_effect = errors.PatientNotFound
+
+    query = f"""
+        {{
+            patient(id: 1) {{
+                first_name
+                last_name
+           }}
+        }}
+    """
+    data = {
+        "query": query
+    }
+    success, response = graphql_sync(schema, data)
+    
+    assert success
+    assert response["data"]["patient"] is None
 
 
 @pytest.fixture(scope="module")
--- patient/tests/test_service.py
+++ patient/tests/test_service.py
@@ -6,19 +6,19 @@ from hypothesis.strategies import integers
 from patient import service, domain
 
 
+@patch("patient.service.Session")
 @given(integers(min_value=1))
-def test__get_patient_by_id__returns_patient_if_exists(patient_id: int):
-    with patch("patient.service.Session") as mock_session:
-        patient_model = Mock(
-            id=patient_id,
-            first_name="Chris",
-            last_name="Ramacciotti",
-        )
-        mock_session.return_value.query.return_value.filter.return_value.one.return_value = patient_model
-        patient = service.get_patient_by_id(patient_id)
+def test__get_patient_by_id__returns_patient_if_exists(mock_session, patient_id: int):
+    patient_model = Mock(
+        id=patient_id,
+        first_name="Chris",
+        last_name="Ramacciotti",
+    )
+    mock_session.return_value.query.return_value.filter.return_value.one.return_value = patient_model
+    patient = service.get_patient_by_id(patient_id)
 
-        assert patient == domain.Patient(
-            id=patient_id,
-            first_name=patient_model.first_name,
-            last_name=patient_model.last_name,
-        )
+    assert patient == domain.Patient(
+        id=patient_id,
+        first_name=patient_model.first_name,
+        last_name=patient_model.last_name,
+    )
